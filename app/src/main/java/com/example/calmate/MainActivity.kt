3f21b899b096837f4d2cf5550e51b3c831301430
m3na0 <m3na02@gmail.com>
2024-03-16 20:05:33 +0200

    Created the Welcome Screen

---
+++ app/src/main/java/com/example/calmate/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.calmate
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val loginButton = findViewById<Button>(R.id.login_btn)
+        loginButton.setOnClickListener {
+            val Intent = Intent(this,Login::class.java)
+            startActivity(Intent)
+        }
+
+        val registerButton = findViewById<Button>(R.id.register_btn)
+        registerButton.setOnClickListener {
+            val Intent = Intent(this,Register::class.java)
+            startActivity(Intent)
+        }
+    }
+}
