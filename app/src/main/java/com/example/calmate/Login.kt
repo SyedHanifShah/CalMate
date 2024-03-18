a99caa050867a2de85a07ab98847c6bb765d062a
m3na0 <m3na02@gmail.com>
2024-03-18 14:00:14 +0200

    Implement login functionality in Login activity

--- app/src/main/java/com/example/calmate/Login.kt
+++ app/src/main/java/com/example/calmate/Login.kt
@@ -1,11 +1,42 @@
 package com.example.calmate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        emailEditText = findViewById(R.id.email_address)
+        passwordEditText = findViewById(R.id.password)
+        loginBtn = findViewById(R.id.login_btn2)
+
+        loginBtn.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.toString()
+
+            if (isValidCredentials(email, password)){
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this, "Invalid email or password", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
+
+    private fun isValidCredentials(email: String, password: String): Boolean{
+        return email == "admin" && password == "password"
+    }
+
 }
